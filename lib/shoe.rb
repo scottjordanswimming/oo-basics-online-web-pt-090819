8b1823e17d4392b9ffa76dd51a90bb825afa90e1
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-25 21:21:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,5 @@
 class Shoe
-  attr_accessor :brand, :color, :material, :condition, :size
+  attr_accessor :brand, :color, :material, :condition, :size, :cobble
   
  def initialize(brand)
    @brand = brand
