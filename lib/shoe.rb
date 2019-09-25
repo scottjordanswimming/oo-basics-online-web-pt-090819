1bf6b83b66de510f302b0ed1b0d5b2a02714f650
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-25 21:18:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,7 @@ class Shoe
   attr_accessor :brand, :color, :material, :condition
   
  
-  def initialize(title)
-    @title = title
-  end
  
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+
  
 end
